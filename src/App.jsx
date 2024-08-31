de4e628461181e988069337d27032f30e9795a79
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-08-30 22:29:06 -0500

    Followed instructors guide to the usercard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import react from "react";
-import "./App.css";
+import "./user.css";
+import user from "./user.json";
 import { UserCard } from "./UserCard";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <UserCard
-        name="Kathy Xiong"
-        age="26"
-        phoneNumber="(920)-888-8888"
-        address="123 Otter Street"
-      />
-    </div>
+    <UserCard
+      name={user.name}
+      phoneNumber={user.phoneNumber}
+      age={user.age}
+      address={user.address}
+    />
   );
 }
-
-export default App;
